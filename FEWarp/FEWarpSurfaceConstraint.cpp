55bb0e4f937b4d512b8f3c35830eb84876a6ff34
SteveMaas1978 <steve.maas@utah.edu>
2012-09-17 20:19:59 +0000

    FEWarp: started working on a surface-based warping algorithm.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEWarp@3819 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEWarp/FEWarpSurfaceConstraint.cpp
@@ -0,0 +1,71 @@
+#include "stdafx.h"
+#include "FEWarpSurfaceConstraint.h"
+#include <FECore/FEElasticMaterial.h>
+
+//=============================================================================
+FEWarpSurface::FEWarpSurface()
+{
+}
+
+//-----------------------------------------------------------------------------
+void FEWarpSurface::Init()
+{
+	// create a new surface
+
+	// allocate and initialize closest-point structure
+
+}
+
+//-----------------------------------------------------------------------------
+double FEWarpSurface::Project(vec3d r)
+{
+	return 0;
+}
+
+//=============================================================================
+BEGIN_PARAMETER_LIST(FEWarpImageConstraint, FEWarpConstraint);
+	ADD_PARAMETER(m_tmp    , FE_PARAM_STRING  , "template");
+	ADD_PARAMETER(m_trg    , FE_PARAM_STRING  , "target"  );
+	ADD_PARAMETER(m_k      , FE_PARAM_DOUBLE  , "penalty" );
+	ADD_PARAMETER(m_blaugon, FE_PARAM_BOOL    , "laugon"  );
+	ADD_PARAMETER(m_altol  , FE_PARAM_DOUBLE  , "altol"   );
+END_PARAMETER_LIST();
+
+//-----------------------------------------------------------------------------
+FEWarpSurfaceConstraint::FEWarpSurfaceConstraint(FEModel* pfem) : FEWarpConstraint(pfem)
+{
+	m_ptmp = 0;
+	m_ptrg = 0;
+}
+
+//-----------------------------------------------------------------------------
+void FEWarpSurfaceConstraint::Init()
+{
+}
+
+//-----------------------------------------------------------------------------
+vec3d FEWarpSurfaceConstraint::wrpForce(FEMaterialPoint& mp)
+{
+	FEElasticMaterialPoint& pt = *mp.ExtractData<FEElasticMaterialPoint>();
+
+	// get the initial and current position
+	vec3d r0 = pt.r0;
+	vec3d rt = pt.rt;
+
+	vec3d qt, qs;
+
+	double T = m_ptmp->Project(r0);
+	double S = m_ptrg->Project(rt);
+
+	// get the target gradient
+	// NOTE: this gradient must be perpendicular to the target surface at the closest point
+	vec3d nS;
+
+	return nS*(T - S);
+}
+
+//-----------------------------------------------------------------------------
+mat3ds FEWarpSurfaceConstraint::wrpStiffness(FEMaterialPoint& mp)
+{
+	return mat3ds();
+}
---
+++ FEWarp/FEWarpSurfaceConstraint.h
@@ -0,0 +1,50 @@
+#include "FEWarpConstraint.h"
+#include <FECore/FESurface.h>
+#include <FECore/FEClosestPointProjection.h>
+
+//-----------------------------------------------------------------------------
+class FEWarpSurface
+{
+public:
+	FEWarpSurface();
+	~FEWarpSurface(){}
+
+	// Initialization
+	void Init();
+
+	// project a point onto the surface
+	double Project(vec3d r);
+
+protected:
+	FEMesh	m_msh;	// the actual surface mesh
+
+	FESurface*	m_psurf;	// surface definition of mesh
+
+	FEClosestPointProjection*	m_cpp;	// closest point projector
+};
+
+//-----------------------------------------------------------------------------
+// Warp constraint between two surfaces
+class FEWarpSurfaceConstraint : public FEWarpConstraint
+{
+public:
+	FEWarpSurfaceConstraint(FEModel* pfem);
+	~FEWarpSurfaceConstraint() {}
+
+	// Initialization
+	void Init();
+
+public:
+	//! Calculate the force at a material point
+	vec3d wrpForce(FEMaterialPoint& pt);
+
+	//! calculate the stiffness at a material point
+	mat3ds wrpStiffness(FEMaterialPoint& pt);
+
+protected:
+	FEWarpSurface*	m_ptmp;
+	FEWarpSurface*	m_ptrg;
+
+public:
+	DECLARE_PARAMETER_LIST();
+};
