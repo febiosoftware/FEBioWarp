76e251b5d5591a85ff3116f6c5fa98944e967c8f
SteveMaas1978 <steve.maas@utah.edu>
2013-02-25 22:27:32 +0000

    FEWarp: moved windows include header inside ifdef clauses.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEWarp@4176 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEWarp/dllmain.cpp
+++ FEWarp/dllmain.cpp
@@ -1,5 +1,9 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#ifdef WIN32
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -17,3 +21,4 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	return TRUE;
 }
 
+#endif // WIN32
