6b7277b382277dce0ab4ddda8ad97f8e4dcd0cf7
SteveMaas1978 <steve.maas@utah.edu>
2014-03-06 21:28:47 +0000

    FEWarp: implemented new factory interface.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEWarp@5670 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEWarp/FEWarp.cpp
+++ FEWarp/FEWarp.cpp
@@ -3,99 +3,41 @@
 
 #include "stdafx.h"
 #include "FECore/FECoreKernel.h"
-#include "FECore/FECoreFactory.h"
 #include "FEWarpImageConstraint.h"
 #include "FEWarpSurfaceConstraint.h"
 #include "FEWarpPlot.h"
 
-#ifdef WIN32
-#define DLL_EXPORT __declspec(dllexport)
-#else
-#define DLL_EXPORT
-#endif
-
 FECoreKernel* pFEBio;
 
 //-----------------------------------------------------------------------------
-class FEWarpImageConstraintFactory : public FECoreFactory
-{
-public:
-	FEWarpImageConstraintFactory() : FECoreFactory(FENLCONSTRAINT_ID, "warp-image"){}
-	void* Create(FEModel* pfem) { return new FEWarpImageConstraint(pfem); }
-};
-
-FEWarpImageConstraintFactory warp_image_factory;
-
-//-----------------------------------------------------------------------------
-class FEWarpMeshConstraintFactory : public FECoreFactory
-{
-public:
-	FEWarpMeshConstraintFactory() : FECoreFactory(FENLCONSTRAINT_ID, "warp-mesh"){}
-	void* Create(FEModel* pfem) { return new FEWarpSurfaceConstraint(pfem); }
-};
-
-FEWarpMeshConstraintFactory warp_mesh_factory;
+//! Factory classes for this plugin.
+FEPluginFactory_T<FEWarpImageConstraint  , FENLCONSTRAINT_ID> warp_image_factory   ("warp-image");
+FEPluginFactory_T<FEWarpSurfaceConstraint, FENLCONSTRAINT_ID> warp_mesh_factory    ("warp-mesh" );
+FEPluginFactory_T<FEPlotTemplate         , FEPLOTDATA_ID    > plot_template_factory("template"  );
+FEPluginFactory_T<FEPlotTarget           , FEPLOTDATA_ID    > plot_target_factory  ("target"    );
+FEPluginFactory_T<FEPlotEnergy           , FEPLOTDATA_ID    > plot_energy_factory  ("energy"    );
+FEPluginFactory_T<FEPlotForce            , FEPLOTDATA_ID    > plot_force_factory   ("warp force");
 
 //-----------------------------------------------------------------------------
-class FEPlotTemplateFactory : public FECoreFactory
-{
-public:
-	FEPlotTemplateFactory() : FECoreFactory(FEPLOTDATA_ID, "template"){}
-	void* Create(FEModel* pfem) { return new FEPlotTemplate(pfem); }
-};
-
-FEPlotTemplateFactory plot_template_factory;
-
-//-----------------------------------------------------------------------------
-class FEPlotTargetFactory : public FECoreFactory
-{
-public:
-	FEPlotTargetFactory() : FECoreFactory(FEPLOTDATA_ID, "target"){}
-	void* Create(FEModel* pfem) { return new FEPlotTarget(pfem); }
-};
-
-FEPlotTargetFactory plot_target_factory;
-
-//-----------------------------------------------------------------------------
-class FEPlotEnergyFactory : public FECoreFactory
-{
-public:
-	FEPlotEnergyFactory() : FECoreFactory(FEPLOTDATA_ID, "energy"){}
-	void* Create(FEModel* pfem) { return new FEPlotEnergy(pfem); }
-};
-
-FEPlotEnergyFactory plot_energy_factory;
-
-//-----------------------------------------------------------------------------
-class FEPlotForceFactory : public FECoreFactory
-{
-public:
-	FEPlotForceFactory() : FECoreFactory(FEPLOTDATA_ID, "warp force"){}
-	void* Create(FEModel* pfem) { return new FEPlotForce(pfem); }
-};
-
-FEPlotForceFactory plot_force_factory;
-
-//-----------------------------------------------------------------------------
-extern "C" DLL_EXPORT void PluginInitialize(FECoreKernel& febio)
+FECORE_EXPORT void PluginInitialize(FECoreKernel& febio)
 {
 
 }
 
 //-----------------------------------------------------------------------------
-extern "C" DLL_EXPORT void PluginCleanup()
+FECORE_EXPORT void PluginCleanup()
 {
 
 }
 
 //-----------------------------------------------------------------------------
-extern "C" DLL_EXPORT int PluginNumClasses()
+FECORE_EXPORT int PluginNumClasses()
 {
 	return 6;
 }
 
 //-----------------------------------------------------------------------------
-extern "C" DLL_EXPORT FECoreFactory* PluginGetFactory(int i)
+FECORE_EXPORT FECoreFactory* PluginGetFactory(int i)
 {
 	switch (i)
 	{
