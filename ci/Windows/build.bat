8469d247b6641c9f016d29f80a650262116543e0
michaelrossherron <michaelrossherron@gmail.com>
2025-06-17 11:57:23 -0600

    Added ci and workflow files

---
+++ ci/Windows/build.bat
@@ -0,0 +1,16 @@
+call "%ONEAPI_ROOT%\setvars.bat"
+set /a "PROC=%NUMBER_OF_PROCESSORS%"
+cmake -version
+
+SET STARTDIR=%cd%
+SET SCRIPTDIR=%~dp0
+
+cd %SCRIPTDIR%\..\..\
+
+cmake -L . -B cmbuild ^
+  -DFEBio_SDK=%STARTDIR%\febio4-sdk
+
+cd cmbuild
+msbuild /P:Configuration=Release /P:WarningLevel=0 /m:%PROC% ALL_BUILD.vcxproj
+cd %STARTDIR%
+exit 0
---
+++ ci/Windows/scp-to-repo.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+scp cmbuild/Release/*.dll repo:~/update2/FEBioStudio2Dev/Windows/stage/bin
